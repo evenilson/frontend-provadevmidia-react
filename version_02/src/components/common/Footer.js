2527d88db55a5701eb3ee32d9a1d09e990d10bb3
evenilsonlp <evenilsonlp@gmail.com>
2021-03-09 21:12:05 -0300

    Criando o component Footer

---
+++ version_02/src/components/common/Footer.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/components/common/Footer.module.css'
+
+export function Footer() {
+    return(
+        <div className={styles.footerContainer}>
+
+        </div>
+    );
+}
