c6db7c07767186e859af6a78c17b12591ee88cd8
evenilsonlp <evenilsonlp@gmail.com>
2021-03-09 21:08:28 -0300

    Criando o componente NavigatorBar

---
+++ version_02/src/components/common/NavigatorBar.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/components/common/NavigatorBar.module.css'
+
+export function NavigatorBar() {
+    return(
+        <div className={styles.navigatorBarContainer}>
+
+        </div>
+    );
+}
