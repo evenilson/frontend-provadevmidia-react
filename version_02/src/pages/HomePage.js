9a9e061167c2c2500d73c0fba21677b592bed7a8
evenilsonlp <evenilsonlp@gmail.com>
2021-03-10 23:50:57 -0300

    Criando o Footer da página, finalizado

--- version_02/src/pages/HomePage.js
+++ version_02/src/pages/HomePage.js
@@ -1,10 +1,12 @@
 
+import { Footer } from '../components/common/Footer';
 import {NavigatorBar} from '../components/common/NavigatorBar';
 
 export function HomePage() {
     return(
         <>
             <NavigatorBar />
+            <Footer />
         </>
     );
 }
