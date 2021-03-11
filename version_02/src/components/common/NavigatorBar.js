7d5535c4254e2af75b89faaa958b72dbd08dcc67
evenilsonlp <evenilsonlp@gmail.com>
2021-03-11 00:03:19 -0300

    Criando estrutura da barra de navegação

--- version_02/src/components/common/NavigatorBar.js
+++ version_02/src/components/common/NavigatorBar.js
@@ -1,9 +1,16 @@
+import { NavLink } from 'react-router-dom'
+
 import styles from '../../styles/components/common/NavigatorBar.module.css'
 
 export function NavigatorBar() {
     return(
-        <div className={styles.navigatorBarContainer}>
-            NavigatorBar
-        </div>
+        <header className={styles.navigatorBarContainer}>
+            <NavLink exact to='/'><span>LOGO</span></NavLink>
+            <div>
+                <NavLink to='/'>cadastrar notícia</NavLink>
+                <NavLink to='/'>exibir noticia</NavLink>
+                <input type="text" />
+            </div>
+        </header>
     );
 }
