f6c1827f6c6b32c63141a7f3efb254cea9e11c46
evenilsonlp <evenilsonlp@gmail.com>
2021-04-28 23:43:29 -0300

    Mudando cor ao ficar com botão hambuerguer e tirando bug de key

--- version_02/src/components/Content.js
+++ version_02/src/components/Content.js
@@ -8,306 +8,60 @@ import { Card } from "./Card";
 export function Content() {
     let cardsInfo = [
         {
+            id: 1,
             title: "Titulo card 1",
             info: "Informação card 1"
         },
         {
+            id: 2,
             title: "Titulo card 2",
             info: "Informação card 2"
         },
         {
+            id: 3,
             title: "Titulo card 3",
             info: "Informação card 3"
         },
         {
+            id: 4,
             title: "Titulo card 4",
             info: "Informação card 4"
         },
         {
+            id: 5,
             title: "Titulo card 5",
             info: "Informação card 5"
         },
         {
-            title: "Titulo card 6",
-            info: "Informação card 6"
+            id: 6,
+            title: "Titulo card 1",
+            info: "Informação card 1"
         },
         {
-            title: "Titulo card 7",
-            info: "Informação card 7"
+            id: 7,
+            title: "Titulo card 2",
+            info: "Informação card 2"
         },
         {
-            title: "Titulo card 8",
-            info: "Informação card 8"
+            id: 8,
+            title: "Titulo card 3",
+            info: "Informação card 3"
         },
         {
-            title: "Titulo card 9",
-            info: "Informação card 9"
+            id: 9,
+            title: "Titulo card 4",
+            info: "Informação card 4"
         },
         {
-            title: "Titulo card 10",
-            info: "Informação card 10"
+            id: 10,
+            title: "Titulo card 5",
+            info: "Informação card 5"
         },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        },
-        {
-            title: "Titulo card 10",
-            info: "Informação card 10"
-        }
         
     ]
 
     const cardsMake = cardsInfo.map((card) =>
-        <Card title={card.title} info={card.info}/>
+        <Card key={card.id} title={card.title} info={card.info}/>
     );
     
     return (
